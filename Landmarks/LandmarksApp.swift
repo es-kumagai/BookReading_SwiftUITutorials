bc795ddc24a56cc2307a1bbc938b32168beec7be
Tomohiro Kumagai <live@swift-stage.com>
2025-09-05 21:01:30 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by SwiftStage on 2025/09/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
